a197369c7970aff1e99fd10c3969326ed62cd240
Xandi <santosal2018@gmail.com>
2025-01-19 20:40:29 -0500

    create a MyWork component

--- MyWebsite/src/App.jsx
+++ MyWebsite/src/App.jsx
@@ -6,11 +6,10 @@ import linkedinLogoDark from "./assets/linkedin-dark.svg";
 import MyResume from "./assets/Alexandre-Santos-Resume.pdf";
 import downArrowLight from "./assets/down-arrow-light.svg";
 import downArrowDark from "./assets/down-arrow-dark.svg";
-import techsyncLogo from "./assets/TechSync-Logo.png";
 
 import Toggle from './components/Toggle/Toggle';
 import NavBar from './components/NavBar/NavBar';
-
+import MyWork from './components/MyWork/MyWork';
 
 import { useState, useEffect, useRef } from 'react';
 
@@ -58,39 +57,7 @@ function App() {
       <div className='scroll'>
         <button className='down-arrow-button' onClick={scrollToMoreInfo}><img src={isDark ? downArrowLight : downArrowDark} className='down-arrow' alt='arrow pointing downwards' /></button>
       </div>
-      <div className='techsync-buffer'></div>
-      <section 
-        className='techsync columns-2'
-        ref={techSyncRef}
-      >
-
-        <div className='section-column sm:w-1/2 w-full'>
-          <div className='flex justify-center'>
-            <a className='project-title' target="_blank" href='https://tech-sync.io/talent'>Tech-Sync</a>
-          </div>
-          <div className='project-text'>
-            <h3 className='project-subtitle' style={{color: "var(--techsync-color-purple)"}}>Description</h3>
-            <p>
-              Unlock the next level of your tech career with Tech-Sync, where we simplify your job search into a streamlined experience based on probability. 
-              Our advanced matchmaking system will connect you with startups that align with your unique skills and career goals. 
-              Say goodbye to traditional job sourcing and hello to a tailored path toward your dream role.
-            </p>
-          </div>
-          <div className='project-text'>
-            <h3 className='project-subtitle' style={{color: "var(--techsync-color-blue)"}}>App Overview</h3>
-            <p>Tech-Sync is a MERN Stack application co-developed by me and another programmer. 
-              As graduates of web development bootcamps, we experienced firsthand how challenging it can be to break into the tech industry, 
-              especially for those with non-traditional backgrounds. We built this app to enhance our skills, improve our job prospects, 
-              and address a market need by connecting tech professionals with potential employers in a unique way.
-            </p>           
-          </div>
-        </div>
-        <div className='section-column sm:w-1/2 w-full flex items-center justify-center' style={{flexDirection: "column"}}>
-          <img className="project-image max-h-full" src={techsyncLogo} alt='Logo for TechSync'></img>
-          <a className='website-link' target="_blank" href='https://tech-sync.io/talent'>Visit Website</a>
-        </div>
-      </section>
-      {/* <div className='footer-buffer'></div> */}
+      <MyWork scrollLocation={techSyncRef}/>
       <footer className='columns-2'>
         <a aria-label="View my Github profile" target="_blank" href="https://github.com/santos-al" ><img className="svg-links" src={isDark ? githubLogoLight : githubLogoDark} alt="Github logo"/></a>
         <a aria-label="View my Linkedin profile" target="_blank" href="https://www.linkedin.com/in/santos-alexandre1"><img className="svg-links" src={isDark ? linkedinLogoLight : linkedinLogoDark} alt="Linkedin logo"/></a>
--- MyWebsite/src/components/MyWork/MyWork.jsx
+++ MyWebsite/src/components/MyWork/MyWork.jsx
@@ -1,1 +1,46 @@
-// Template for each project displayed
+import techsyncLogo from "../../assets/TechSync-Logo.png";
+import PropTypes from 'prop-types'; // Import PropTypes
+
+export default function MyWork({ scrollLocation }) {
+  return (
+    <>
+      <div className='techsync-buffer'></div>
+      <section 
+        className='techsync columns-2'
+        ref={scrollLocation}
+      >
+        <div className='section-column sm:w-1/2 w-full'>
+          <div className='flex justify-center'>
+            <a className='project-title' target="_blank" href='https://tech-sync.io/talent'>Tech-Sync</a>
+          </div>
+          <div className='project-text'>
+            <h3 className='project-subtitle' style={{color: "var(--techsync-color-purple)"}}>Description</h3>
+            <p>
+              Unlock the next level of your tech career with Tech-Sync, where we simplify your job search into a streamlined experience based on probability. 
+              Our advanced matchmaking system will connect you with startups that align with your unique skills and career goals. 
+              Say goodbye to traditional job sourcing and hello to a tailored path toward your dream role.
+            </p>
+          </div>
+          <div className='project-text'>
+            <h3 className='project-subtitle' style={{color: "var(--techsync-color-blue)"}}>App Overview</h3>
+            <p>Tech-Sync is a MERN Stack application co-developed by me and another programmer. 
+              As graduates of web development bootcamps, we experienced firsthand how challenging it can be to break into the tech industry, 
+              especially for those with non-traditional backgrounds. We built this app to enhance our skills, improve our job prospects, 
+              and address a market need by connecting tech professionals with potential employers in a unique way.
+            </p>           
+          </div>
+        </div>
+        <div className='section-column sm:w-1/2 w-full flex items-center justify-center' style={{flexDirection: "column"}}>
+          <img className="project-image max-h-full" src={techsyncLogo} alt='Logo for TechSync'></img>
+          {/* <a className='website-link' target="_blank" href='https://tech-sync.io/talent'>Visit Website</a> */}
+        </div>
+      </section>
+    </>
+  )
+}
+
+
+// Add PropTypes validation
+MyWork.propTypes = {
+  scrollLocation: PropTypes.object.isRequired, // Validate scrollLocation as an object
+};
